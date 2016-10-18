865c3ea6b208ef32bb030b776e6dfc1c483bedef
Mayank Purohit <mayankpurohit@outlook.com>
2016-10-17 21:31:41 -0400

    date time format

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -8,12 +8,31 @@ angular.module('kaplanApp', ['angular.filter'])
 		  }
 
 		$scope.toLocaleDate = function(e) {
-		    var dateTime = e.time.substring(0,10) + "T" + e.time.substring(11,20);
-		    var newDate = new Date(dateTime);
-		    e.date = newDate.toLocaleDateString();
-		    console.log(e.date);
+		    var date = e.time.substring(0,10);
+		    var time = e.time.substring(11,20);
+
+		    var newDate = new Date(date);
+		   
+		    var weekday = new Array("Sun", "Mon", "Tue", "Wed",
+                    "Thu", "Fri", "Sat");
+
+		    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", 
+		    "September", "October", "November", "December");
+
+		   
+		    var daysDisplay = weekday[newDate.getDay()];
+		    var monthDisplay = months[newDate.getMonth()];
+		    var year = newDate.getFullYear();
+
+		    var displayDate = daysDisplay + "," + monthDisplay + " " + newDate.getDate() + "," + year;	
+		    
+		    var dat = new Date(displayDate);
+		    
+		    e.date = dat.toLocaleDateString();
+
 		    return e;
-		};
+		}
+
 
 		$scope.fetchData = function() {
 			$http.get('/getData')
@@ -21,7 +40,7 @@ angular.module('kaplanApp', ['angular.filter'])
 	  			$scope.objectList = data.sort(function(a,b){
 				  // Turn your strings into dates, and then subtract them
 				  // to get a value that is either negative, positive, or zero.
-				  return new Date(a.time.substring(0,10) + "T" + b.time.substring(11,20)) - new Date(b.time.substring(0,10) + "T" + a.time.substring(11,20));
+				  return new Date(a.time.substring(0,10) + "T" + b.time.substring(11,20)) - new Date(b.time.substring(0,10) + "T" + b.time.substring(11,20));
 				});
 	  			console.log("hello");
 	  			//console.log(data);
